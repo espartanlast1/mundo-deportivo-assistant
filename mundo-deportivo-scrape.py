08920e7eb2d911b0807d684b2011529fe1d61dbb
Gerónimo Basso <geronimobasso@Geronimos-MacBook-Pro.local>
2023-10-22 11:53:11 +0200

    ETL of first two functions

--- mundo-deportivo-scrape.py
+++ mundo-deportivo-scrape.py
@@ -83,60 +83,45 @@ def login_fantasy_mundo_deportivo():
 
     return driver
 
+
 def scrape_market_section_fantasy():
     driver = login_fantasy_mundo_deportivo()
-    wait_1 = WebDriverWait(driver, 3)
 
     # Select the markets section, wait ten seconds as it usually takes some time to load the page.
     driver.get("https://mister.mundodeportivo.com/market")
 
-    # Get the table with all the player markets information.
-    market_players_table = wait_1.until(
-        ec.element_to_be_clickable(
-            (
-                By.ID,
-                'list-on-sale'
-            )
-        )
-    )
+    # Get the players data table.
+    market_players_table = driver.find_element(By.ID, 'list-on-sale')
+
+    # Select each player.
+    market_players_info = market_players_table.find_elements(By.CLASS_NAME,'player-row')
 
     # Create an array to save players info.
     players = []
 
-    # Separate every item in a new array everytime there's a break line.
-    players_market_information_array = market_players_table.text.split('\n')
+    for market_player_info in market_players_info:
+        # Split the text to create a list of player information.
+        raw_player_information = market_player_info.text.split('\n')
 
-    """
-    As all the information is in a single array, we need to classify correctly depending on each player. 
-    Each players information consists of 9 columns, so we will individually trim 9 columns of the market array 
-    each time and create a new array each player with all his information.
-    """
-    for i in range(0, len(players_market_information_array), 9):
-        # list slicing to get 9 elements, then the other 9 and so on.
-        player_information = players_market_information_array[i:i + 9]
-        players.append(player_information)
+        # Clean the data.
+        cleaned_player_information = [item.replace("↑", "").replace("↓", "").replace(",", ".") for item in
+                                      raw_player_information]
+
+        players.append(cleaned_player_information)
 
     # ------ Start process to save all the information in a CSV. ------
 
-    # Create the name of the csv
-    current_datetime = datetime.now()
-
-    # Format the name of the CSV with date and time.
-    market_time = current_datetime.strftime('%Y-%m-%d %H:%M')
-    market_time = market_time.replace(" ", "-")
-
     # Create first row of the CSV file.
-    market_structure_header = ['Seller and time', 'Full name', 'Market value', 'Average value',
+    market_structure_header = ['Points', 'Full name', 'Market value', 'Average value',
                                'Ante penultimate match score', 'Penultimate match score',
-                               'Last match score', 'Sale', 'Time Stamp']
+                               'Last match score', 'Attempt to buy', 'Time Stamp']
 
     # Get the name of the CSV file together.
-    file_name = 'data/fantasy-players-in-market.csv'
+    file_name = 'data/league/fantasy-players-in-market.csv'
 
     # Check if the file exists
     file_exists = os.path.exists(file_name)
 
-    # Create a CSV with all the previous information mentioned.
     with open(file_name, 'a' if file_exists else 'w', newline='') as archivo_csv:
         writer = csv.writer(archivo_csv)
 
@@ -144,147 +129,58 @@ def scrape_market_section_fantasy():
         if not file_exists:
             writer.writerow(market_structure_header)
 
-        for player in players:
-            # Add the current timestamp to each player's data
-            player_data = player + [datetime.now().strftime('%Y-%m-%d %H:%M:%S')]
-            writer.writerow(player_data)
+        # Append the players
+        writer.writerows(players)
 
     driver.quit()
 
 
-def scrape_personal_team_fantasy(email, password):
-    driver = webdriver.Chrome()
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
+def scrape_personal_team_fantasy():
+    driver = login_fantasy_mundo_deportivo()
 
-    mundo_deportivo_liga_fantasy = "https://mister.mundodeportivo.com/new-onboarding/auth/email"
+    driver.get("https://mister.mundodeportivo.com/team")
 
-    driver.get(mundo_deportivo_liga_fantasy)
-    wait1 = WebDriverWait(driver, 10000)
-    wait2 = WebDriverWait(driver, 10000)
+    team_players_table = driver.find_element(By.CLASS_NAME, 'player-list')
 
-    # Wait for the cookies to appear and click the button to accept them.
-    button_cookies = wait1.until(
-        ec.element_to_be_clickable(
-            (
-                By.ID,
-                'didomi-notice-agree-button'
-            )
-        )
-    )
+    # Select each player.
+    team_players_info = team_players_table.find_elements(By.CLASS_NAME,'info')
 
-    button_cookies.click()
-
-    # Enter the email and password.
-    email_input = driver.find_element(By.ID, 'email')
-    email_input.send_keys(email)
-
-    password_input = driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div/form/div[2]/input')
-    password_input.send_keys(password)
-
-    # Click on the login button.
-    submit_button = wait1.until(
-        ec.element_to_be_clickable(
-            (
-                By.XPATH,
-                '//*[@id="app"]/div/div[2]/div/form/div[3]/button'
-            )
-        )
-    )
-    submit_button.click()
-
-    # Special wait to skip the first tutorial.
-    """"
-    skip_button = wait1.until(
-        ec.element_to_be_clickable(
-            (
-                By.CLASS_NAME,
-                'btn-tutorial-skip'
-            )
-        )
-    )
-    skip_button.click()
-    """
-
-    # Select the markets section, wait ten seconds as it usually takes some time to load the page.
-    team_selection = wait2.until(
-        ec.element_to_be_clickable(
-            (
-                By.XPATH,
-                '//*[@id="content"]/header/div[2]/ul/li[3]/a'
-            )
-        )
-    )
-    team_selection.click()
-
-    # Get the table with all the player markets information.
-    team_table = wait2.until(
-        ec.element_to_be_clickable(
-            (
-                By.XPATH,
-                '//*[@id="content"]/div[2]/div[4]/ul'
-            )
-        )
-    )
-    market_players_table = driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div[4]/ul')
-
-    # Separate every item in a new array everytime there's a break line.
-    players_market_information_array = market_players_table.text.split('\n')
-
-    """
-    As all the information is in a single array, we need to classify correctly depending on each player. 
-    Each players information consists of 9 columns, so we will individually trim 9 columns of the market array 
-    each time and create a new array each player with all his information.
-    """
-    # create the list to fill with the players information and the start index to loop through the array.
+    # Create an array to save players info.
     players = []
-    start_index = 0
 
-    # Loop until there's no "Vender" left
-    while 'Vender' in players_market_information_array[start_index:]:
-        # Get the index of the next "Vender" and add 1 to get the end index.
-        end_index = players_market_information_array.index('Vender', start_index) + 1
-        # Get the player information from the start index to the end index.
-        player_information = players_market_information_array[start_index:end_index]
+    for team_player_info in team_players_info:
+        # Split the text to create a list of player information.
+        raw_player_information = team_player_info.text.split('\n')
 
-        players.append(player_information)
-        start_index = end_index
+        # Clean the data.
+        cleaned_player_information = [item.replace("↑", "").replace("↓", "").replace(",", ".") for item in
+                                      raw_player_information]
 
-    # Start process to save all the information in a CSV.
+        players.append(cleaned_player_information)
 
-    # Create the name of the csv
-    current_datetime = datetime.now()
-
-    # Format the name of the CSV with date and time.
-    market_time = current_datetime.strftime('%Y-%m-%d %H:%M')
-    market_time = market_time.replace(" ", "-")
+    # ------- Start process to save all the information in a CSV. --------
 
     # Create first row of the CSV file.
-    market_structure_header = ['Puntuacion', 'Nombre', 'Valor mercado', 'Promedio valor',
-                               'Antepenultimo partido puntuacion', 'Penultimo partido puntuacion',
-                               'Ultimo partido puntuacion', 'Time Stamp']
+    team_players_header = ['Name', 'Market value', 'Average value',
+                           'Ante penultimate match score', 'Penultimate match score',
+                           'Last match score']
 
     # Get the name of the CSV file together.
-    file_name = 'data/fantasy-team-data.csv'
+    file_name = 'data/league/fantasy-personal-team-data.csv'
 
     # Check if the file exists
     file_exists = os.path.exists(file_name)
 
     # Create a CSV with all the previous information mentioned.
     with open(file_name, 'a' if file_exists else 'w', newline='') as archivo_csv:
+
         writer = csv.writer(archivo_csv)
 
-        print(market_structure_header)
         # Write the header
         if not file_exists:
-            writer.writerow(market_structure_header)
+            writer.writerow(team_players_header)
 
-        for player in players:
-            # Add the current timestamp to each player's data
-            player.pop()
-            player_data = player + [datetime.now().strftime('%Y-%m-%d %H:%M:%S')]
-            print(player_data)
-            writer.writerow(player_data)
+        writer.writerows(players)
 
     driver.quit()
 
@@ -989,15 +885,8 @@ def scrape_la_liga_standings(api_key):
 
 
 if __name__ == '__main__':
-    with open('config.json') as config_file:
-        config = json.load(config_file)
-
-    email_fantasy = config['email']
-    password_fantasy = config['password']
-    api_football = config['api-football']
-
-    scrape_market_section_fantasy()
-    #scrape_personal_team_fantasy(email_fantasy, password_fantasy)
+    #scrape_market_section_fantasy()
+    scrape_personal_team_fantasy()
     #scrape_la_liga_standings(api_football)
     #scrape_all_players_fantasy(email_fantasy,password_fantasy)
     #scrape_players_stats_fantasy(email_fantasy, password_fantasy)

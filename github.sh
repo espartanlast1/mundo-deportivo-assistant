31b5ecc509ab1d01745adb86c0402379b09b5501
Manuel C <FridtjofElineVanKann@protonmail.com>
2023-11-15 12:31:43 +0100

    Test Commit

---
+++ github.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if a commit message is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Please provide a commit message."
+    exit 1
+fi
+
+# Add all changes (including untracked files) to the staging area
+git add .
+
+# Commit the changes with a GPG signature and the provided commit message
+git commit -S -m "$@"
+
+# Pull the latest changes from the remote "main" branch
+git pull new_data_fantasy
+
+# Add all changes (including untracked files) to the staging area
+git add .
+
+# Commit the changes with a GPG signature and the provided commit message
+git commit -S -m "$@"
+
+# Push the committed changes to the "main" branch
+git push --set-upstream origin new_data_fantasy
